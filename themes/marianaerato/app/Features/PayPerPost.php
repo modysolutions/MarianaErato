92e77cd2e573250cf8e40dea3ee8c54ff6842d22
Javier Troya <javiertroya@gmail.com>
2026-09-14 11:14:51 +0200

    Anticipated content.

--- themes/marianaerato/app/Features/PayPerPost.php
+++ themes/marianaerato/app/Features/PayPerPost.php
@@ -4,15 +4,15 @@ namespace App\Features;
 
 trait PayPerPost
 {
-    public function get_posts_linked_to_product($product_id): array
+    public function get_posts_linked_to_product($product_id, array $statuses = ['publish']): array
     {
         $args = [
-            'post_type'  => 'post',
-            'post_status' => 'publish',
+            'post_type' => 'post',
+            'post_status' => $statuses,
             'meta_query' => [
                 [
-                    'key'     => 'wc_pay_per_post_product_ids',
-                    'value'   => '"' . $product_id . '"',
+                    'key' => 'wc_pay_per_post_product_ids',
+                    'value' => '"'.$product_id.'"',
                     'compare' => 'LIKE',
                 ],
             ],
@@ -25,12 +25,12 @@ trait PayPerPost
     public function get_product_linked_to_post($post_id): array
     {
         $args = [
-            'post_type'  => 'post',
+            'post_type' => 'post',
             'post_status' => 'publish',
             'meta_query' => [
                 [
-                    'key'     => 'wc_pay_per_post_product_ids',
-                    'value'   => '"' . $post_id . '"',
+                    'key' => 'wc_pay_per_post_product_ids',
+                    'value' => '"'.$post_id.'"',
                     'compare' => 'LIKE',
                 ],
             ],
@@ -49,8 +49,10 @@ trait PayPerPost
             $sponsorship_page_id = get_field('sponsorship_purchase_redirect_page', 'option');
             if ($sponsorship_page_id) {
                 $translated_page_id = apply_filters('wpml_object_id', $sponsorship_page_id, 'page', true, $lang);
+
                 return get_permalink($translated_page_id ?? $sponsorship_page_id);
             }
+
             return wc_get_page_permalink('myaccount');
         }
 
@@ -58,6 +60,7 @@ trait PayPerPost
         $linked_post_id = count($linked_products) > 1 ? reset($linked_products)->ID : $product_id;
 
         $translated_post_id = apply_filters('wpml_object_id', $linked_post_id, 'post', true, $lang);
+
         return get_permalink($translated_post_id ?? $linked_post_id);
     }
 }
--- themes/marianaerato/woocommerce/content-product.php
+++ themes/marianaerato/woocommerce/content-product.php
@@ -1,10 +1,13 @@
 <?php
+
+use App\Hooks\AnticipatedAccess;
+
 defined('ABSPATH') || exit;
 
 global $product, $post;
 $product_id = get_the_ID();
 // Check if the product is a valid WooCommerce product and ensure its visibility before proceeding.
-if (!is_a($product, WC_Product::class) || !$product->is_visible()) {
+if (! is_a($product, WC_Product::class) || ! $product->is_visible()) {
     return;
 }
 $regular_price = get_post_meta($product_id, '_regular_price', true);
@@ -13,27 +16,35 @@ $current_price = number_format(get_post_meta($product_id, '_price', true), 2, ',
 $currency = get_woocommerce_currency();
 $currency_symbol = get_woocommerce_currency_symbol($currency) ?? '';
 $image = wp_get_attachment_image_src(get_post_thumbnail_id($product_id), 'single-post-thumbnail');
-if(str_contains($image[0] ?? '', 'marianaerato.com')) {
-	$image = str_replace('wordpress/', '', $image);
+if (str_contains($image[0] ?? '', 'marianaerato.com')) {
+    $image = str_replace('wordpress/', '', $image);
 }
 $text = get_field('buy_this_post_text', 'option') ?? 'Buy this post for %s';
 $color = get_field('sales_text_color', $product_id);
-$non_sponsors_available_date = get_field('non_sponsors_available_date', $product_id, false);
 $current_time = time();
 $is_available = true;
-$release_timestamp = time();
+$release_timestamp = null;
 
-if ($non_sponsors_available_date) {
-    $release_timestamp = strtotime($non_sponsors_available_date);
-
-    $early_access_hours = get_user_meta(get_current_user_id(), 'me_early_access_to_blog_posts', true);
-
-    if ($early_access_hours) {
-        $user_allowed_timestamp = $release_timestamp - (intval($early_access_hours) * 3600);
-        $is_available = ($current_time >= $user_allowed_timestamp);
-    } else {
-        $is_available = ($current_time >= $release_timestamp);
+$linked_release_timestamp = AnticipatedAccess::resolve_release_timestamp((int) $product_id);
+if ($linked_release_timestamp !== null) {
+    $release_timestamp = $linked_release_timestamp;
+} else {
+    $non_sponsors_available_date = get_field('non_sponsors_available_date', $product_id, false);
+    if ($non_sponsors_available_date) {
+        $release_timestamp = strtotime($non_sponsors_available_date);
     }
+}
+
+if ($release_timestamp) {
+    $early_access_hours = AnticipatedAccess::user_anticipated_hours(get_current_user_id());
+    if ($early_access_hours > 0) {
+        $user_allowed_timestamp = $release_timestamp - $early_access_hours * HOUR_IN_SECONDS;
+        $is_available = $current_time >= $user_allowed_timestamp;
+    } else {
+        $is_available = $current_time >= $release_timestamp;
+    }
+} else {
+    $release_timestamp = $current_time;
 }
 ?>
 <style>.mm-background { background-image: url(<?php echo $image[0] ?? ''; ?>); }</style>
@@ -42,18 +53,18 @@ if ($non_sponsors_available_date) {
   <div class='mm-background-overlay'>&nbsp;</div>
   <h4 class='mm-buy_this'>
       <?php
-      echo sprintf(__($text, APP_THEME_DOMAIN), $currency_symbol . $current_price);
+      echo sprintf(__($text, APP_THEME_DOMAIN), $currency_symbol.$current_price);
 
-      if ($is_available) {
-          $checkout_url = get_field('link_buy_now_button_url', $product_id);
-          $checkout_text = __('Buy now', APP_THEME_DOMAIN);
-      } else {
-          $checkout_url = 'javascript:void(0)';
-          $formatted_date = date_i18n('F jS, Y', $release_timestamp);
-          $available_on_text = get_field('non_sponsor_sales_text', 'option') ?? 'Available on %s';
-          $checkout_text = sprintf(__($available_on_text, APP_THEME_DOMAIN), $formatted_date);
-      }
-      ?>
+if ($is_available) {
+    $checkout_url = get_field('link_buy_now_button_url', $product_id);
+    $checkout_text = __('Buy now', APP_THEME_DOMAIN);
+} else {
+    $checkout_url = 'javascript:void(0)';
+    $formatted_date = date_i18n('F jS, Y', $release_timestamp);
+    $available_on_text = get_field('non_sponsor_sales_text', 'option') ?? 'Available on %s';
+    $checkout_text = sprintf(__($available_on_text, APP_THEME_DOMAIN), $formatted_date);
+}
+?>
   </h4>
   <a href="<?php echo $checkout_url ?>" class="mm-add_to_cart"><?php echo $checkout_text ?></a>
 </div>
@@ -63,13 +74,13 @@ if ($non_sponsors_available_date) {
 <?php
 $product_template = get_field('pay_per_post_product_template', 'option');
 if ($product_template) {
-    echo do_shortcode('[elementor-template id="' . $product_template . '"]');
+    echo do_shortcode('[elementor-template id="'.$product_template.'"]');
 }
 ?>
 <script>
     (function ($) {
         $('body').on('click', '.mm-add_to_cart', function () {
-            localStorage.setItem('mm_redirect_to', '<?php echo get_permalink($post->ID);?>');
+            localStorage.setItem('mm_redirect_to', '<?php echo get_permalink($post->ID); ?>');
         })
     })(jQuery)
 </script>
